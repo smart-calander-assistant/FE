57f728ad1a8653e2d64776bfa6e3cc17a678ce5d
hansg931 <hansg0509@cau.ac.kr>
2023-11-21 05:03:18 +0900

    feat: 캘린더, 추천 페이지 작성

---
+++ src/mordal/CalendarPlanModal.js
@@ -0,0 +1,70 @@
+import {styled} from "styled-components";
+import PlanCard from "../component/PlanCard";
+import {IoClose} from "react-icons/io5";
+
+export default function CalendarPlanModal({setModalOpen, plans}) {
+    if (!setModalOpen || !plans || plans.length === 0) {
+        return null;
+    }
+
+    return (
+        <ViewContainer>
+            <RootContainer>
+                <ModalContainer>
+                    <ModalTitle>
+                        <ModalDetail>내 정보</ModalDetail>
+                        <IoClose
+                            size={'2rem'}
+                            onClick={() => setModalOpen(false)}
+                        />
+                    </ModalTitle>
+                    {plans.map((plan) => (
+                        <PlanCard
+                            key={plan.id}
+                            id={plan.id}
+                            start_time={plan.start_time}
+                            end_time={plan.end_time}
+                            place={plan.place}
+                            title={plan.title}
+                        />
+                    ))}
+                </ModalContainer>
+            </RootContainer>
+        </ViewContainer>
+    )
+}
+
+const ViewContainer = styled.div`
+  z-index: 1;
+  position: absolute;
+`;
+
+const RootContainer = styled.div`
+  position: fixed;
+  inset: 0;
+  background-color: rgb(0 0 0 / 30%);
+  -webkit-tap-highlight-color: transparent;
+  justify-content: center;
+  padding: 12vh 1.5rem;
+`;
+
+const ModalContainer = styled.div`
+  position: relative;
+  background: white;
+  border-radius: 0.5rem;
+  transition: all 400ms ease-in-out 2s;
+  padding: 2rem;
+`;
+
+const ModalTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 3rem;
+  align-items: center;
+  color: black;
+`;
+
+const ModalDetail = styled.p`
+  font-weight: 600;
+  font-size: 1.5rem;
+`;
